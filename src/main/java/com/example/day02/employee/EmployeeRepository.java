2e0044e86f701928100016d032fccb49515ff73d
DESKTOP-1N9MQ9B\Nattapol Puninagoon <nattapol.p@cdg.co.th>
2021-10-01 16:41:52 +0700

    add EmployeeRepositoryTest

---
+++ src/main/java/com/example/day02/employee/EmployeeRepository.java
@@ -0,0 +1,8 @@
+package com.example.day02.employee;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository  extends JpaRepository<Employee,Integer> {
+
+
+}
